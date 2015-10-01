287055ecfef526270b5accdfbb0e9c7c868beb07
Sylvia <sylviakieha@gmail.com>
2015-10-01 19:50:55 +0300

    created user form and profile pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
   before_save :create_avatar_url
-  attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
   has_secure_password
   before_validation :prep_email
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+  validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/, :multiline => true }
 
   private
 
