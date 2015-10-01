e1072282202affe32646ac7ff36c84badc8b9dd7
Sylvia <sylviakieha@gmail.com>
2015-10-01 23:54:14 +0300

    created user relationship setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
   has_many :ribits
   before_save :create_avatar_url
   before_validation :prep_email
@@ -16,4 +20,13 @@ class User < ActiveRecord::Base
   def create_avatar_url
     self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
   end
+
+  def following? user
+    self.followeds.include? user
+end
+
+def follow user
+    Relationship.create follower_id: self.id, followed_id: user.id
+end
+
 end
