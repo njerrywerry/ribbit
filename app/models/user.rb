f33e3ea484fb2cf6b8c72aaba2b9798b67e20d33
Sylvia <sylviakieha@gmail.com>
2015-10-04 14:21:20 +0300

    added following feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
-  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, class_name: "Relationship", foreign_key: "follower_id"
   has_many :followers, through: :follower_relationships
   has_many :followeds, through: :followed_relationships
   has_many :ribits
@@ -10,6 +10,14 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/, :multiline => true }
+
+    def following? user
+      self.followeds.include? user
+  end
+
+  def follow user
+      Relationship.create follower_id: self.id, followed_id: user.id
+  end
 
   private
 
@@ -20,13 +28,5 @@ class User < ActiveRecord::Base
   def create_avatar_url
     self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
   end
-
-  def following? user
-    self.followeds.include? user
-end
-
-def follow user
-    Relationship.create follower_id: self.id, followed_id: user.id
-end
 
 end
