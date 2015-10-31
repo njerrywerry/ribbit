d3ce21721992474e35cd36301f1bf3923f71cf03
Sylvia <sylviakieha@gmail.com>
2015-11-01 00:22:03 +0300

    generated test data with factories

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,12 @@
 require "rails_helper"
 
 describe User do
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
   it "is invalid without a name"
   it "is invalid without a username"
   it "is invalid without an email"
+  it "is invalid without a password"
 
 end
