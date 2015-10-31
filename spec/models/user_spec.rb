b61e39d133895842c17734c5100cc8d2dc13ce19
Sylvia <sylviakieha@gmail.com>
2015-11-01 00:29:53 +0300

    tested validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,17 @@ describe User do
   it "has a valid factory" do
     expect(FactoryGirl.create(:user)).to be_valid
   end
-  it "is invalid without a name"
-  it "is invalid without a username"
-  it "is invalid without an email"
-  it "is invalid without a password"
+  it "is invalid without a name" do
+    expect(FactoryGirl.build(:user, name: nil)).to be_invalid
+  end
+  it "is invalid without a username" do
+    expect(FactoryGirl.build(:user, username: nil)).to be_invalid
+  end
+  it "is invalid without an email" do
+    expect(FactoryGirl.build(:user, email: nil)).to be_invalid
+  end
+  it "is invalid without a password" do
+    expect(FactoryGirl.build(:user, password: nil)).to be_invalid
+  end
 
 end
