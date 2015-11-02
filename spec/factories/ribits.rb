37ce1d8b5a34e171424fd7a04d551f24b89fd169
Sylvia <sylviakieha@gmail.com>
2015-11-02 12:18:59 +0300

    added tests for presence validation in ribits

--- spec/factories/ribits.rb
+++ spec/factories/ribits.rb
@@ -4,4 +4,8 @@ FactoryGirl.define do
   factory :ribit do |f|
     f.content { Faker::Lorem.characters(140) }
   end
+
+  factory :invalid_ribit, parent: :ribit do |f|
+    f.content nil
+  end
 end
--- spec/models/ribit_spec.rb
+++ spec/models/ribit_spec.rb
@@ -4,6 +4,9 @@ describe Ribit do
   it "has a valid factory" do
     expect(FactoryGirl.create(:ribit)).to be_valid
   end
+  it "is invalid without content" do
+    expect(FactoryGirl.build(:ribit, content: nil)).to be_invalid
+  end
   it "has a length of 140 characters" do
     expect(FactoryGirl.build(:ribit, content: 140)).to be_valid
   end
